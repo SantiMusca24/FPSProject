dc803d6e72b84d8bf414ad6b07ef21c9dea2e73d
w <manuel.pereiro@davinci.edu.ar>
2024-09-21 17:00:05 -0300

    RELOAD ANIMATION FIX

--- Assets/Scripts/Guns/WeaponSwitching.cs
+++ Assets/Scripts/Guns/WeaponSwitching.cs
@@ -6,10 +6,16 @@ public class WeaponSwitching : MonoBehaviour
 {
 
     [SerializeField] private GameObject _akUI, _pistolUI;
+    [SerializeField] private Animator _akReload, _pistolShoot;
+
 
     static public int selectedWeapon = 0;
     void Start()
     {
+        _akReload.keepAnimatorStateOnDisable = true;
+        _pistolShoot.keepAnimatorStateOnDisable = true;
+
+
         SelectWeapon();
     }
 
@@ -22,43 +28,60 @@ public class WeaponSwitching : MonoBehaviour
         {
             if(selectedWeapon >= transform.childCount - 1) 
             {
+                //_akReload.Play("normalState", 0, 0f);
+                //_pistolShoot.Play("M1911@Fire", 0, 0f);
                 selectedWeapon = 0;
                 BulletCounter._shootCooldown = false;
                 PistolBullets._shootCooldown = false;
             }
             else
             {
+                _akReload.Play("Reload_m4", 0, 0f);
+                //_pistolShoot.Play("normalState", 0, 0f);
                 selectedWeapon++;
                 BulletCounter._shootCooldown = false;
                 PistolBullets._shootCooldown = false;
+
             }
         }
         if (Input.GetAxis("Mouse ScrollWheel") < 0f)
         {
             if (selectedWeapon <= 0)
             {
+                _akReload.Play("Reload_m4", 0, 0f);
+                //_pistolShoot.Play("normalState", 0, 0f);
                 selectedWeapon = transform.childCount -1 ;
                 BulletCounter._shootCooldown = false;
                 PistolBullets._shootCooldown = false;
+
             }
             else
             {
+                //_akReload.Play("normalState", 0, 0f);
+                //_pistolShoot.Play("M1911@Fire", 0, 0f);
                 selectedWeapon--;
                 BulletCounter._shootCooldown = false;
                 PistolBullets._shootCooldown = false;
+
             }
         }
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
+            //_akReload.Play("normalState", 0, 0f);
+            //_pistolShoot.Play("M1911@Fire", 0, 0f);
             selectedWeapon = 0;
             BulletCounter._shootCooldown = false;
             PistolBullets._shootCooldown = false;
+
         }
         if (Input.GetKeyDown(KeyCode.Alpha2)&& transform.childCount >=2)
         {
+            _akReload.Play("Reload_m4", 0, 0f);
+            //_pistolShoot.Play("normalState", 0, 0f);
             selectedWeapon = 1;
             BulletCounter._shootCooldown = false;
             PistolBullets._shootCooldown = false;
+
         }
         if (previousSelectedWeapon != selectedWeapon)
         {
@@ -67,13 +90,17 @@ public class WeaponSwitching : MonoBehaviour
 
         if (selectedWeapon == 0)
         {
+
             _akUI.SetActive(true);
             _pistolUI.SetActive(false);
+
         }
         else if (selectedWeapon == 1)
         {
+
             _akUI.SetActive(false);
             _pistolUI.SetActive(true);
+
         }
     }
 
